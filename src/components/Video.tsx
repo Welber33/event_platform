1bd27fbef0b009dc67846de8af464547544cbc28
Welber33 <jwwork21@gmail.com>
2022-08-17 17:48:32 -0300

    Ignite Lab - Aula 05(Deploy CodeGen)

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,50 +1,21 @@
-import { gql, useQuery } from "@apollo/client";
 import { DefaultUi, Player, Youtube } from "@vime/react";
 import { CaretRight, DiscordLogo, FileArrowDown,Image , Lightning } from "phosphor-react";
 
 import '@vime/core/themes/default.css';
-
-
-const GET_LESSON_BY_SLUG_QUERY = gql`
-  query GetLessonBySlug($slug: String) {
-    lesson(where: {slug: $slug}) {
-      title
-      videoId
-      description
-      teacher {
-        bio
-        avatarURL
-        name
-      }
-    }
-  }
-`
-
-interface GetLessonSlugResponse {
-  lesson: {
-    title: string;
-    videoId: string;
-    description: string;
-    teacher: {
-      bio: string;
-      avatarURL: string;
-      name: string;
-    }
-  }
-}
-
+import { useGetLessonBySlugQuery } from "../graphql/generated";
+ 
 interface VideoProps {
   lessonSlug: string;
 }
 
 export function Video(props: VideoProps){
-  const { data } = useQuery<GetLessonSlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+  const { data } = useGetLessonBySlugQuery({
     variables: {
       slug: props.lessonSlug,
     }
   })
 
-  if(!data){
+  if(!data || !data.lesson){
     return (
       <div className="flex-1">
         <p>Carregando...</p>
@@ -73,7 +44,8 @@ export function Video(props: VideoProps){
               {data.lesson.description}
             </p>
 
-            <div className="flex items-center gap-4 mt-6">
+            {data.lesson.teacher && (
+              <div className="flex items-center gap-4 mt-6">
               <img 
               className="h-16 w-16 rounded-full border-2 border-blue-500"
               src={data.lesson.teacher.avatarURL} 
@@ -85,6 +57,7 @@ export function Video(props: VideoProps){
                 </div>
               
             </div>
+            )}
           </div>
 
           <div className="flex flex-col gap-4">
